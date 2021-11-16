cf9defca5bf5c57230a11da820f5c56ee7c5f9ae
pruisshinta <94352308+pruisshinta@users.noreply.github.com>
2021-11-17 00:14:38 +0700

    Add files via upload

---
+++ 2001_Shinta.py
@@ -0,0 +1,9 @@
+nama = "Pruis Shinta Reza Elbas"
+Nim = "212410102001"
+prodi = "Teknologi Informasi"
+asal = "Jember"
+hobi = "menggambar"
+
+print("Halo semua, perkenalkan namaku", nama, "dengan NIM", Nim, "prodi", prodi)
+print("Aku berasal dari Kota", asal, "dan hobiku adalah", hobi)
+print("Salam kenal semuanya, yuk kalau mau mutualan") 
